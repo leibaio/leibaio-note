1a9e687502e0cfa429ba2a14ba46fa212e0b0410
leibaio <leibaio@qq.com>
2024-06-08 22:34:39 +0800

    feat: demo test

--- README.md
+++ README.md
@@ -24,8 +24,8 @@ For example:
 
 This extension contributes the following settings:
 
-* `myExtension.enable`: Enable/disable this extension.
-* `myExtension.thing`: Set to `blah` to do something.
+- `myExtension.enable`: Enable/disable this extension.
+- `myExtension.thing`: Set to `blah` to do something.
 
 ## Known Issues
 
@@ -53,19 +53,61 @@ Added features X, Y, and Z.
 
 Ensure that you've read through the extensions guidelines and follow the best practices for creating your extension.
 
-* [Extension Guidelines](https://code.visualstudio.com/api/references/extension-guidelines)
+- [Extension Guidelines](https://code.visualstudio.com/api/references/extension-guidelines)
 
 ## Working with Markdown
 
 You can author your README using Visual Studio Code. Here are some useful editor keyboard shortcuts:
 
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux).
-* Toggle preview (`Shift+Cmd+V` on macOS or `Shift+Ctrl+V` on Windows and Linux).
-* Press `Ctrl+Space` (Windows, Linux, macOS) to see a list of Markdown snippets.
+- Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux).
+- Toggle preview (`Shift+Cmd+V` on macOS or `Shift+Ctrl+V` on Windows and Linux).
+- Press `Ctrl+Space` (Windows, Linux, macOS) to see a list of Markdown snippets.
 
 ## For more information
 
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
+- [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
+- [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
 
 **Enjoy!**
+
+```json
+{
+  "name": "leibaio-note",
+  "publisher": "leibaio",
+
+  "description": "",
+  "version": "0.0.1",
+  "engines": {
+    "vscode": "^1.90.0"
+  },
+  "categories": [
+    "Other"
+  ],
+  "activationEvents": [],
+  "main": "./out/extension.js",
+  "contributes": {
+    "commands": [
+      {
+        "command": "leibaio-note.helloWorld",
+        "title": "Hello World"
+      }
+    ]
+  },
+  ...
+}
+
+```
+
+其中：
+
+- **name**和**publisher**是必填项，分别声明插件的名称和发布者，在插件市场，`<publisher>.<name>`是插件的唯一识别码
+
+- **version**表示插件版本号
+
+- **displayName**、**description**和**categories**，分别表示插件在插件市场中显示的名称、描述和分类
+
+- **activationEvents**和**contributes**分别描述 Activation Events 和 Contribution Points，是插件的激活方式，声明命令为 `Hello World`，在命令面板出现，命令 id 为`leibaio-note.helloWorld`
+
+- **main**描述插件的入口文件
+
+- **engines.vscode**表示插件支持的 vscode 版本，`^1.90.0`表示支持 1.90.0 及以上的版本
--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,135 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+class Note extends vscode.TreeItem {
+  constructor(
+    public readonly label: string,
+    public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+    public readonly command?: vscode.Command
+  ) {
+    super(label, collapsibleState);
+    this.command = command;
+  }
+}
+
+class NotesTreeDataProvider implements vscode.TreeDataProvider<Note> {
+  getTreeItem(element: Note): vscode.TreeItem {
+    return element;
+  }
+
+  getChildren(element?: Note): Thenable<Note[]> {
+    if (element) {
+      if (element.label === "目录1") {
+        return Promise.resolve([
+          new Note(`笔记1`, vscode.TreeItemCollapsibleState.None, {
+            command: 'leibaio-note.showNote', // 使用命令ID
+            title: "", // 用不到title参数，因为这是点击时的命令，不是按钮
+            arguments: ['笔记1的内容'] // 可传递给命令的参数，例如笔记内容或ID
+          }),
+          new Note(`笔记2`, vscode.TreeItemCollapsibleState.None),
+        ]);
+      }
+      // 判断是否是目录节点，然后返回相应的笔记
+      if (element.label.startsWith("目录")) {
+        const directoryIndex = parseInt(element.label.slice(-1));
+        // 假设每个目录都有两个笔记
+        return Promise.resolve([
+          new Note(`笔记${(directoryIndex - 1) * 2 + 1}`, vscode.TreeItemCollapsibleState.None),
+          new Note(`笔记${(directoryIndex - 1) * 2 + 2}`, vscode.TreeItemCollapsibleState.None),
+        ]);
+      } else {
+        // 如果已经是笔记节点了，就没有子节点
+        return Promise.resolve([]);
+      }
+    } else {
+      // 返回两个目录作为根节点
+      return Promise.resolve([
+        new Note('目录1', vscode.TreeItemCollapsibleState.Collapsed),
+        new Note('目录2', vscode.TreeItemCollapsibleState.Collapsed),
+      ]);
+    }
+  }
+}
+
+// activate() 插件激活时候，插件入口
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "leibaio-note" is now active!');
+  console.log('Congratulations, your extension "leibaio-note" is now active!');
+  // 命令 id 必须与 package.json 中的 command 字段匹配，相当于绑定了一个事件监听器，命令触发(如下leibaio-note.helloWorld)就执行
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('leibaio-note.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from leibaio-note!');
-	});
+  // Hello World 命令
+  const disposableHelloWorld = vscode.commands.registerCommand('leibaio-note.helloWorld', () => {
+    // 绑定的function，执行内容
+    vscode.window.showInformationMessage('Hello World from leibaio-note hi hi hi!');
+  });
 
-	context.subscriptions.push(disposable);
+  // Open Note 命令
+  let disposableOpenNote = vscode.commands.registerCommand('leibaio-note.openNote', () => {
+    // 创建Webview Panel
+    const panel = vscode.window.createWebviewPanel(
+      'leibaioNote', // Identifies the type of the webview. Used internally
+      'Leibaio Note', // Title of the panel displayed to the user
+      vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+      {} // Webview options. More on these later.
+    );
+
+    // 设置Webview的HTML内容
+    panel.webview.html = getWebviewContent();
+  });
+
+  // 注册打开笔记的命令
+  let disposableShowNote = vscode.commands.registerCommand('leibaio-note.showNote', (noteContent: string) => {
+    // 创建Webview Panel
+    const panel = vscode.window.createWebviewPanel(
+      'note', // Webview类型
+      'Note', // Webview标题
+      vscode.ViewColumn.One, // 在编辑器的哪一列中显示新的webview面板
+      {} // Webview选项
+    );
+
+    // 设置Webview的HTML内容
+    panel.webview.html = getNoteWebviewContent(noteContent);
+  });
+
+  // 注册TreeView数据提供者
+  const notesTreeViewProvider = new NotesTreeDataProvider();
+  vscode.window.createTreeView('noteTreeView', { treeDataProvider: notesTreeViewProvider });
+
+  context.subscriptions.push(disposableHelloWorld);
+  context.subscriptions.push(disposableOpenNote);
+  context.subscriptions.push(disposableShowNote);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
+
+// 生成显示在Webview中的HTML内容
+function getWebviewContent() {
+  return `<!DOCTYPE html>
+          <html lang="en">
+          <head>
+              <meta charset="UTF-8">
+              <meta name="viewport" content="width=device-width, initial-scale=1.0">
+              <title>Leibaio Note</title>
+          </head>
+          <body>
+              <h1>Leibaio Note</h1>
+              <p>这里显示Leibaio的笔记内容。</p>
+          </body>
+          </html>`;
+}
+
+// 生成显示笔记内容的Webview HTML
+function getNoteWebviewContent(noteContent: string) {
+  return `<!DOCTYPE html>
+          <html lang="en">
+          <head>
+              <meta charset="UTF-8">
+              <meta name="viewport" content="width=device-width, initial-scale=1.0">
+              <title>Note</title>
+          </head>
+          <body>
+              <h1>Note</h1>
+              <p>${noteContent}</p>
+          </body>
+          </html>`;
+}
